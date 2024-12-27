4d5b602c8f288556b777d1a9813b670079d682d4
fevunge <developervunge@gmail.com>
2024-12-27 23:29:22 +0100

    instance PrismaClient

--- src/routes/record.ts
+++ src/routes/record.ts
@@ -1,4 +1,7 @@
 import { FastifyInstance } from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();  
 
 export async function recordRoutes(app: FastifyInstance) {
     app.get('/', () => {
