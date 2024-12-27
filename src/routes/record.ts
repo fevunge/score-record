3d1b0aa1434c717fa2252e3314744d266e1b51a2
fevunge <developervunge@gmail.com>
2024-12-28 00:32:51 +0100

    add POST route and schemas for doc

--- src/routes/record.ts
+++ src/routes/record.ts
@@ -1,10 +1,45 @@
-import { FastifyInstance } from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { FastifyTypedInstance } from '../types';
+import { z } from 'zod';
 
 const prisma = new PrismaClient();  
 
-export async function recordRoutes(app: FastifyInstance) {
-    app.get('/', () => {
-        return ('oi')
+export async function recordRoutes(app: FastifyTypedInstance) {
+    app.get('/records', {
+		schema : {
+			tags: ['records'],
+			description: 'list all records'
+		}
+	},
+	async () => {	
+        const records = await prisma.record.findMany(
+            {
+                orderBy: {
+                    score: 'desc',
+                },
+            }
+        )
+
+        return (records.map((record) => {
+            return {
+                id: record.id,
+                player: record.player,
+                score: record.score.toString() 
+            }
+        }))
     })
+
+	app.post('/records', {
+		schema: {
+			tags: ['records'],
+			description: 'save a new record',
+			body : {
+				player: z.string(),
+				score: z.number(),
+			},
+		},
+	}, 
+	async (request, reply) => {
+		return reply.status(201).send()
+	})
 }
