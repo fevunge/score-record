2a8064b32bc6779a22f9314d9312922769ea4880
fevunge <developervunge@gmail.com>
2024-12-28 00:34:12 +0100

    implement cors, validator and serializer compiler, swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { recordRoutes } from "./routes/record";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifySwagger from "@fastify/swagger";
 
-const server = fastify()
+const server = fastify().withTypeProvider<ZodTypeProvider>()
+
+server.register(fastifyCors, { origin: "*" })
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+server.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "online-record",
+            version: "1.0.1",
+        }
+    }
+})
+server.register(fastifySwaggerUi, {
+    routePrefix: "/doc",
+})
 server.register(recordRoutes)
 
 server.listen({
